c155cf867cfc7dbef2c7e396dcedf1fdc06c5cb4
Hanxi Zhang <hanxizh@amazon.com>
2026-01-23 22:47:55 +0000

    Passing 0 for restore command to load all the fields
    Signed-off-by: Hanxi Zhang <hanxizh@amazon.com>

--- src/cluster.c
+++ src/cluster.c
@@ -269,7 +269,7 @@ void restoreCommand(client *c) {
     }
 
     long long now = commandTimeSnapshot();
-    obj = rdbLoadObject(type, &payload, objectGetVal(key), c->db->id, NULL, RDBFLAGS_NONE, now);
+    obj = rdbLoadObject(type, &payload, objectGetVal(key), c->db->id, NULL, RDBFLAGS_NONE, 0);
     if (obj == NULL) {
         addReplyError(c, "Bad data format");
         return;
