33c5e6dd654b24f57eeafab55a54ec4da8467bf3
Hanxi Zhang <hanxizh@amazon.com>
2026-03-12 13:58:12 -0700

    Added dummy test for valkey and moduleapi
    Signed-off-by: Hanxi Zhang <hanxizh@amazon.com>

--- tests/unit/moduleapi/dummy-flaky.tcl
+++ tests/unit/moduleapi/dummy-flaky.tcl
@@ -1,15 +1,15 @@
-start_server {tags {"dummy"}} {
-    test "dummy-flaky - basic SET and GET" {
+start_server {tags {"modules dummy"}} {
+    test "moduleapi-dummy - basic SET and GET" {
         r SET mykey myvalue
         assert_equal [r GET mykey] "myvalue"
     }
 
-#    test "dummy-flaky - intentional failure" {
+#    test "moduleapi-dummy - intentional failure" {
 #        r SET mykey myvalue
 #        assert_equal [r GET mykey] "wrongvalue"
 #    }
 
-    test "dummy-flaky - another passing test" {
+    test "moduleapi-dummy - another passing test" {
         r SET counter 0
         r INCR counter
         assert_equal [r GET counter] "1"
