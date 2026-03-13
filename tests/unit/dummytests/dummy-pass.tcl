ce8ff1712b96f118bf20326ff1c7b6f797db41b2
Hanxi Zhang <hanxizh@amazon.com>
2026-03-13 15:04:42 -0700

    Added another test
    Signed-off-by: Hanxi Zhang <hanxizh@amazon.com>

---
+++ tests/unit/dummytests/dummy-pass.tcl
@@ -0,0 +1,12 @@
+start_server {tags {"dummy"}} {
+    test "dummy-pass - SET and GET" {
+        r SET testkey testvalue
+        assert_equal [r GET testkey] "testvalue"
+    }
+
+    test "dummy-pass - INCR" {
+        r SET num 10
+        r INCR num
+        assert_equal [r GET num] "99"
+    }
+}
