867eb3d9d8a0a0aeff4cd3118e886a8e43048e93
Hanxi Zhang <hanxizh@amazon.com>
2026-05-12 18:43:49 +0000

    Try another way to verify the code is being executed
    Signed-off-by: Hanxi Zhang <hanxizh@amazon.com>

--- tests/support/util.tcl
+++ tests/support/util.tcl
@@ -1224,9 +1224,9 @@ proc system_backtrace_supported {} {
     }
 
     # Check if built with USE_LIBBACKTRACE (for musl/Alpine)
-    set buildinfo [exec grep -a "USE_LIBBACKTRACE" $::VALKEY_SERVER_BIN]
-    if {$buildinfo ne ""} {
-        error "DEBUG: Found USE_LIBBACKTRACE in binary - intentionally failing to prove this code path is executed"
+    # Look for libbacktrace symbols instead of the macro string
+    if {[catch {exec grep -a "initLibbacktraceFrameState" $::VALKEY_SERVER_BIN} buildinfo] == 0} {
+        error "PROOF: Found initLibbacktraceFrameState symbol - this code path IS being executed on [exec uname -s]"
     }
 
     # libmusl does not support backtrace natively. Also return 0 on
